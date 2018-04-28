d77d9d345b638a5877bf2e5a565f436d3b0e9823
jk <1777980+fallafeljan@users.noreply.github.com>
2018-04-28 14:52:09 +0200

    basic revamps; no yarn; dotenv

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const dotenv = require('dotenv')
 const webpack = require('webpack')
 const autoprefixer = require('autoprefixer')
 
@@ -7,105 +8,119 @@ const StyleExtHtmlWebpackPlugin = require('style-ext-html-webpack-plugin')
 
 const debug = process.env.NODE_ENV !== 'production'
 
-const cssLoaders = [{
-  loader: 'css-loader',
-  options: {
-    discardComments: {
-      removeAll: true
-    }
-  }
-}, {
-  loader: 'postcss-loader',
-  options: {
-    plugins: () => [autoprefixer]
-  }
-}]
+if (debug) {
+  dotenv.config()
+}
+
+const cssLoaders = [
+  {
+    loader: 'css-loader',
+    options: {
+      discardComments: {
+        removeAll: true,
+      },
+    },
+  },
+  {
+    loader: 'postcss-loader',
+    options: {
+      plugins: () => [autoprefixer],
+    },
+  },
+]
 
 const config = {
   entry: [`${__dirname}/app/index.js`],
   output: {
-    path: `${__dirname}/public`,
-    filename: debug ? 'bundle.js' : 'bundle.[hash].js'
+    path: `${__dirname}/dist`,
+    filename: debug ? 'bundle.js' : 'bundle.[hash].js',
   },
 
   devtool: debug ? 'source-map' : false,
 
   performance: {
-    hints: debug ? false : 'warning'
+    hints: debug ? false : 'warning',
   },
 
   stats: {
-    children: false
+    children: false,
   },
 
   module: {
-    rules: [{
-      test: /\.js$/,
-      loader: 'babel-loader'
-    }, {
-      test: /\.css$/,
-      use: debug ?
-        ['style-loader'].concat(cssLoaders) :
-        ExtractTextWebpackPlugin.extract({
-          fallback: 'style-loader',
-          use: cssLoaders
-        })
-    }, {
-      test: /\.html$/,
-      use: [{
-        loader: 'html-loader'
-      }]
-    }, {
-      test: /\.(svg|png)$/,
-      use: {
-        loader: 'url-loader',
-        options: {
-          limit: 10000
-        }
-      }
-    }, {
-      test: /\.otf$/,
-      use: 'file-loader'
-    }]
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: debug
+          ? ['style-loader'].concat(cssLoaders)
+          : ExtractTextWebpackPlugin.extract({
+              fallback: 'style-loader',
+              use: cssLoaders,
+            }),
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(svg|png)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+          },
+        },
+      },
+      {
+        test: /\.otf$/,
+        use: 'file-loader',
+      },
+    ],
   },
 
   devServer: {
-    contentBase: `${__dirname}/public`,
-    port: 3000
+    contentBase: `${__dirname}/dist`,
+    port: 3000,
   },
 
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        EMAIL_ADDRESS: JSON.stringify(process.env.EMAIL_ADDRESS),
+        NODE_ENV: JSON.stringify(debug ? 'development' : 'production'),
+      },
+    }),
+
     new webpack.LoaderOptionsPlugin({
       minimize: !debug,
-      debug
+      debug,
     }),
 
     new HtmlWebpackPlugin({
-      favicon: `${__dirname}/favicon.ico`,
       inject: 'body',
-      template: `${__dirname}/index.html`
-    })
-  ]
+      template: `${__dirname}/index.html`,
+    }),
+  ],
 }
 
 if (!debug) {
   const prodPlugins = [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
-    }),
-
     new ExtractTextWebpackPlugin('layout.css'),
     new StyleExtHtmlWebpackPlugin(!debug),
 
     new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true
-    })
+      sourceMap: true,
+    }),
   ]
 
-  prodPlugins.forEach(plugin =>
-    config.plugins.push(plugin))
+  prodPlugins.forEach(plugin => config.plugins.push(plugin))
 }
 
 module.exports = config
