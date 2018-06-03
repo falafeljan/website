b8cdaabdf0d00bcc6fc350d65b757b247bf17bd6
jk <1777980+fallafeljan@users.noreply.github.com>
2018-06-03 22:40:25 +0200

    layout changes

--- app/components/App.js
+++ app/components/App.js
@@ -1,23 +1,41 @@
 // @flow
 import React from 'react'
+import styled from 'styled-components'
 import {emailAddress} from '../args'
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  padding: 20px;
+`
+
+const Par = styled.p`
+  margin: 0px 0px 20px 0px;
+  line-height: 140%;
+`
 
 export default function App() {
   return (
-    <main>
-      <p>
-        Jan Kaßel is a computer science graduate student at Leipzig University,
-        Germany.
-      </p>
-      <nav role="navigation">
-        <a href={`mailto:${emailAddress}`} className="contact">
-          <em>Contact</em>
-        </a>
+    <Container>
+      <main>
+        <Par>Jan Kaßel</Par>
 
-        <a href="https://github.com/fallafeljan" className="github">
-          GitHub
-        </a>
+        <Par>
+          I currently pursue a master’s degree in Computer Science at Leipzig
+          University, Germany.
+        </Par>
+
+        <Par>
+          My interest lies in digital applications that affect design, society,
+          and research — I strive for artistic simplicity in complex systems
+          while pursueing empowerment of the individual.
+        </Par>
+      </main>
+
+      <nav role="navigation">
+        <a href={`mailto:${emailAddress}`}>Contact</a>
       </nav>
-    </main>
+    </Container>
   )
 }
