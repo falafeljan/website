3c8f9152ac8be69ddbd8cc3e586dece35bb4579e
jk <1777980+falafeljan@users.noreply.github.com>
2019-10-02 20:06:40 +0200

    add sangbleu italic and bold typefaces

--- layout/sang-bleu/index.js
+++ layout/sang-bleu/index.js
@@ -1,11 +1,29 @@
 import {createGlobalStyle} from 'styled-components'
 import regular from './SangBleuRepublic-Regular-WebM.woff'
 import regular2 from './SangBleuRepublic-Regular-WebM.woff2'
+import italic from './SangBleuRepublic-RegularItalic-WebM.woff'
+import italic2 from './SangBleuRepublic-RegularItalic-WebM.woff2'
+import bold from './SangBleuRepublic-Bold-WebM.woff'
+import bold2 from './SangBleuRepublic-Bold-WebM.woff2'
 
 export default createGlobalStyle`
   @font-face {
     font-family: 'SangBleu Republic';
     src: url(${regular2}) format('woff2'),
       url(${regular}) format('woff');
+  }
+  
+  @font-face {
+    font-family: 'SangBleu Republic';
+    font-style: italic;
+    src: url(${italic2}) format('woff2'),
+      url(${italic}) format('woff');
+  }
+  
+  @font-face {
+    font-family: 'SangBleu Republic';
+    font-weight: bold;
+    src: url(${bold2}) format('woff2'),
+      url(${bold}) format('woff');
   }
 `
