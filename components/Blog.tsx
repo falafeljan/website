a60ea868c7f3b5ef8cf03815b7c9b8fbb28d33aa
Jan Kaßel <jan@kassel.works>
2023-02-27 13:23:19 +0100

    fix blog component css

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
-import styled from 'styled-components'
-import Link from '../components/Link'
-import {H2, H4} from '../components/Heading'
+import Link from 'next/link'
 import index from '../log-index.json'
-import {largeWidth} from '../layout/grid'
+import styles from '../layout/Blog.module.css'
 
 const months = [
   'Jan',
@@ -20,64 +18,18 @@ const months = [
   'Dec',
 ]
 
-const PostList = styled.ul`
-  margin: 0;
-  padding: 0;
-  list-style: none;
-`
-
-const PostItem = styled.li`
-  margin: 0 0 10px;
-  padding: 0;
-
-  &:last-child {
-    margin-bottom: 0;
-  }
-
-  @media screen and (min-width: ${largeWidth + 1}px) {
-    margin-bottom: 20px;
-  }
-`
-
-const Post = styled.div`
-  display: flex;
-  flex-flow: row;
-`
-
-const PostDate = styled.time`
-  flex-shrink: 0;
-  display: inline-block;
-  margin: 7px 12px 3px 0;
-
-  font-family: 'iA Writer Quattro', sans-serif;
-  font-size: 60%;
-`
-
-const PostTitle = styled(H4)`
-  display: inline-block;
-  flex-grow: 1;
-  margin: 0;
-
-  font-size: inherit;
-  font-weight: normal;
-`
-
 function LogEntry({title, date, slug}) {
   const postDate = new Date(date)
 
   return (
     <article itemScope itemType="https://schema.org/BlogPosting">
-      <Link
-        href={`/blog/post?slug=${slug}`}
-        as={`/blog/${slug}`}
-        itemProp="url"
-      >
-        <Post>
-          <PostDate datetime={postDate.toISOString()} itemProp="datePublished">
+      <Link href={`/blog/${slug}`} itemProp="url">
+        <div className={styles.post}>
+          <time dateTime={postDate.toISOString()} itemProp="datePublished">
             {months[postDate.getMonth()]} {postDate.getDate()}
-          </PostDate>
-          <PostTitle itemProp="headline">{title}</PostTitle>
-        </Post>
+          </time>
+          <h4 itemProp="headline">{title}</h4>
+        </div>
       </Link>
     </article>
   )
@@ -85,16 +37,16 @@ function LogEntry({title, date, slug}) {
 
 export default function Blog() {
   return (
-    <div className="log-overview">
-      <H2>Research Log</H2>
+    <div>
+      <h2>Research Log</h2>
 
-      <PostList>
+      <ul className={styles.posts}>
         {index.map((entry) => (
-          <PostItem key={entry.slug}>
+          <li key={entry.slug}>
             <LogEntry {...entry} />
-          </PostItem>
+          </li>
         ))}
-      </PostList>
+      </ul>
     </div>
   )
 }
