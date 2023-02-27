3eab8b808b4bfb4f29063198c7c508a230991f97
Jan Kaßel <jan@kassel.works>
2023-02-27 16:30:24 +0100

    add legacy redirect

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ import fm_mdx from 'remark-mdx-frontmatter'
 const withMDX = createMDX({
   extension: /\.mdx?$/,
   options: {
-    remarkPlugins: [gfm, smartypants, fm, [fm_mdx, { name: 'postMeta'}]],
+    remarkPlugins: [gfm, smartypants, fm, [fm_mdx, {name: 'postMeta'}]],
     rehypePlugins: [],
     // If you use `MDXProvider`, uncomment the following line.
     // providerImportSource: "@mdx-js/react",
@@ -22,6 +22,16 @@ const withMDX = createMDX({
 const nextConfig = {
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: '/publications',
+        destination: '/about',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default withMDX(withFonts(nextConfig))
