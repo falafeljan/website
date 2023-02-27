36674357f66786ff418fe363ab4fcb08b4241c00
Jan Kaßel <jan@kassel.works>
2023-02-27 15:57:28 +0100

    fix blog post fetching

--- lib/fetchPost.ts
+++ lib/fetchPost.ts
@@ -1,11 +1,26 @@
+import path from 'path'
+import fs from 'fs/promises'
+import fm from 'front-matter'
 import index from '../log-index.json'
 
-export default async function fetchPost(slug: string) {
+type Meta = {
+  title: string
+  date: Date
+  location?: string
+}
+
+export default async function fetchPost(slug: string): Promise<{
+  body: string
+  meta: Meta
+}> {
   const post = index.find((post) => post.slug === slug)
   if (!post) {
     throw new Error(`Could not find post with slug \`${slug}\`.`)
   }
 
-  const {default: Post, postMeta: meta} = await import(`../blog/${post.id}.md`)
-  return {Post, meta}
+  const base = path.join(process.cwd(), 'blog')
+  const {body, attributes} = fm(
+    await fs.readFile(`${base}/${post.id}.md`, 'utf8'),
+  )
+  return {body, meta: attributes as Meta}
 }
